8399608baecdf1540c5203c25bd50d9a160b570a
youness ajdir <younessajdir44@gmail.com>
2025-03-24 16:44:21 +0000

    first commit

---
+++ projectService/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.PORT
+const host = process.env.HOST
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+const projectRoutes = require('./routes/Projects');
+app.use('/project', projectRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running at http://${host}:${port}`);
+});
---
+++ projectService/routes/Projects.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const Project = require('../models/ProjectModel');
+// const { verifyToken } = require('../middleware/auth');
+
+router.get('/all',  async (req, res) => {
+    try {
+        const projects = await Project.find({});
+        res.json(projects);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/add',  async (req, res) => {
+    try {
+        const newProject = new Project(req.body);
+        const data = await newProject.save();
+        res.status(201).json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.put('/update/:id',  async (req, res) => {
+    try {
+        const projectId = req.params.id;
+        const updatedData = req.body;
+        const data = await Project.findOneAndUpdate({ id: projectId }, updatedData, { new: true });
+
+        if (!data) {
+            return res.status(404).json({ message: 'Projet non trouvé' });
+        }
+
+        res.json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.delete('/delete/:id',  async (req, res) => {
+    try {
+        const projectId = req.params.id;
+        const data = await Project.findOneAndDelete({ id: projectId });
+
+        if (!data) {
+            return res.status(404).json({ message: 'Projet non trouvé' });
+        }
+
+        res.json({ message: 'Projet supprimé avec succès', data });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.get('/search',  async (req, res) => {
+    try {
+        const { name, startDate, endDate, status } = req.query;
+        let filter = {};
+
+        if (name) filter.name = { $regex: name, $options: 'i' };
+        if (startDate) filter.startDate = { $gte: new Date(startDate) };
+        if (endDate) filter.endDate = { $lte: new Date(endDate) };
+        if (status) filter.status = status;
+
+        const projects = await Project.find(filter);
+        res.json(projects);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+
+module.exports = router;
